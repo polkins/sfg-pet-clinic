abae514dd967166dbeddcfbf18b36587a3beb638
Polkins <markina_pln@mail.ru>
2020-09-08 23:37:23 +0300

    adding POJOs.Closes #2.

---
+++ src/main/java/model/Pet.java
@@ -0,0 +1,31 @@
+package model;
+
+public class Pet {
+    private PetType petType;
+    private Owner owner;
+    private String birthday;
+
+    public PetType getPetType() {
+        return petType;
+    }
+
+    public void setPetType(PetType petType) {
+        this.petType = petType;
+    }
+
+    public Owner getOwner() {
+        return owner;
+    }
+
+    public void setOwner(Owner owner) {
+        this.owner = owner;
+    }
+
+    public String getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(String birthday) {
+        this.birthday = birthday;
+    }
+}
